1e274c66736ab5c412967460088a32bd3498ac21
Thomas Johler <thomas.johler@msg.group>
2024-11-21 21:15:20 +0100

    Allow file://-URLs for feeds (mainly for tests).

--- feedcache/requests_dl.py
+++ feedcache/requests_dl.py
@@ -41,6 +41,9 @@ def tmp_downloader(feed, cfg, state, log=LOGGER):
     sess = requests.Session()
     sess.cookies = cookies
 
+    if feed.url.startswith("file://"):
+      sess.mount("file://", LocalFileAdapter())
+
     rsp = sess.get(feed.url, headers=headers, timeout=timeout, proxies=feed.proxies)
 
     if rsp.ok and rsp.text:
@@ -75,3 +78,67 @@ def tmp_downloader(feed, cfg, state, log=LOGGER):
 
   return rc, "{0}: {1}".format(errtext, rc), data
 
+from pathlib import Path
+import io
+
+# https://stackoverflow.com/a/22989322/10545609
+class LocalFileAdapter(requests.adapters.HTTPAdapter):
+  def build_response_from_file(self, request):
+      file_path : Path = Path.from_uri(request.url)
+      if file_path.exists():
+        with open(file_path, 'rb') as file:
+            buff = bytearray(os.path.getsize(file_path))
+            file.readinto(buff)
+            resp = LocalFileAdapter.Resp(buff)
+            r = self.build_response(request, resp)
+      else:
+        r = self.build_response(request, LocalFileAdapter.Resp(b"Not found!", 404))
+      return r
+
+  def send(self, request, stream=False, timeout=None,
+           verify=True, cert=None, proxies=None):
+
+      return self.build_response_from_file(request)
+
+  # https://github.com/ambv/requests-testadapter/blob/master/src/requests_testadapter.py
+  class Resp(io.BytesIO):
+    def __init__(self, stream, status=200, headers=None):
+        self.status = status
+        self.headers = headers or {}
+        self.reason = requests.status_codes._codes.get(
+            status, ['']
+        )[0].upper().replace('_', ' ')
+        io.BytesIO.__init__(self, stream)
+
+    @property
+    def _original_response(self): return self
+    @property
+    def msg(self): return self
+    def read(self, chunk_size, **kwargs): return io.BytesIO.read(self, chunk_size)
+    def info(self): return self
+    def get_all(self, name, default):
+        result = self.headers.get(name)
+        if not result: return default
+        return [result] # pragma: nocover
+    def getheaders(self, name): return self.get_all(name, [])
+    def release_conn(self): self.close()
+
+  try:
+    # Only in Python 3.13+ 
+    from_uri = Path.from_uri
+  except AttributeError: # TODO: REMOVEME # pragma: nocover
+    # https://github.com/python/cpython/pull/107640/files
+    @classmethod
+    def from_uri(cls, uri):
+        """Return a new path from the given 'file' URI."""
+        if not uri.startswith('file:'): raise ValueError(f"URI does not start with 'file:': {uri!r}")
+        path = uri[5:]
+        if path[:3] == '///': path = path[2:]
+        elif path[:12] == '//localhost/': path = path[11:]
+        if path[:3] == '///' or (path[:1] == '/' and path[2:3] in ':|'): path = path[1:]
+        if path[1:2] == '|': path = path[:1] + ':' + path[2:]
+        from urllib.parse import unquote_to_bytes
+        path = cls(os.fsdecode(unquote_to_bytes(path)))
+        if not path.is_absolute(): raise ValueError(f"URI is not absolute: {uri!r}")
+        return path
+    Path.from_uri = from_uri
